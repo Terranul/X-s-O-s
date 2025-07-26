35d4468b5eff83c4146d64e404e89fe4da2b0fe2
Terranul <163475387+Terranul@users.noreply.github.com>
2025-07-26 10:19:29 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,17 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-        // Board board = new Board();
-        // board.move(3, true);
-        // board.move(6, true);
-        // board.move(8, true);
-        // System.out.println(board.hasPlayerWon(true));
-        // Engine engine = new Engine(true);
-        // System.out.println(engine.run());
-
         UI ui = new UI();
         ui.run();
-
-
     }
 }
